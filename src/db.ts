6a34af2429e9e22925341379443760cf219cd966
dlevenson44 <daniellevenson4@gmail.com>
2023-11-24 09:28:20 -0500

    fix up linting/prettier/tsconfig rules

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,16 @@
-import { Pool } from 'pg'
 import dotenv from 'dotenv'
+import { Pool } from 'pg'
 
 dotenv.config()
 
 const DB = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 })
 
 export default DB
