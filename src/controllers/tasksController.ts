5d8b17b8acac5da1a1a68bbfe99c778e2ba8ba92
dlevenson44 <daniellevenson4@gmail.com>
2023-11-25 22:01:41 -0500

    add error handling

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -3,6 +3,7 @@ import type { Request, Response } from 'express'
 import isEmpty from 'lodash/isEmpty'
 import type { QueryResult } from 'pg'
 
+import { generateServerError, generateClientError } from '../helpers/errors'
 import {
   fetchAllTasks,
   fetchTaskById,
@@ -17,77 +18,81 @@ export const getTasks = (_req: Request, res: Response) =>
     .then((result: QueryResult<Task>) =>
       res.status(200).json({ data: result.rows })
     )
-    .catch((err) => {
-      res.status(500).json({ err, message: 'Error fetching all tasks' })
-    })
+    .catch((err) => generateServerError({ res, action: 'fetching', err }))
 
 export const getTaskById = (req: Request, res: Response) => {
   const id = parseInt(String(req.params.id))
   fetchTaskById(id)
     .then((result: QueryResult) =>
       isEmpty(result.rows)
-        ? res.status(404).json({
-            message: `Task ID ${id} was not found in DB`,
-          })
+        ? generateClientError({ res, action: 'fetching', id, status: 404 })
         : res.status(200).json({ data: result.rows[0] })
     )
-    .catch((err) =>
-      res
-        .status(500)
-        .json({ err, message: `Error fetching task with id ${id}` })
-    )
+    .catch((err) => generateServerError({ res, action: 'fetching', err, id }))
 }
 
 export const postTask = (req: Request<PostTaskPayload>, res: Response) => {
   const { title, description } = req.body
 
   if (!title || !description || title.length > 255) {
-    res.status(400).json({
-      message:
-        'Bad payload: title AND description are required to create new task',
+    const messageDetails = title.length
+      ? 'Title length too long'
+      : 'Missing Title and/or Description field'
+    generateClientError({
+      res,
+      action: 'creating',
+      status: 400,
+      messageDetails,
     })
   } else {
     createTask({ title, description })
       .then((result: QueryResult<Task>) => res.status(201).json({ result }))
-      .catch((err) => {
-        res.status(500).json({ err, message: 'Error creating task' })
-      })
+      .catch((err) => generateServerError({ res, action: 'creating', err }))
   }
 }
 
 export const putTask = (req: Request<Task>, res: Response) => {
   const id = parseInt(String(req.params.id))
   const { title, description, completed } = req.body
-  updateTask({ title, description, completed, id })
-    .then((result: QueryResult<Task>) =>
-      isEmpty(result.rows)
-        ? res.status(404).json({
-            message: `Task ID ${id} was not found in DB`,
-          })
-        : res.status(200).json({ result })
-    )
-    .catch((err) => {
-      res.status(500).json({
-        err,
-        message: `Error updating task with id ${id}`,
-      })
+
+  if (!title || !description || !completed || title.length > 255) {
+    const messageDetails = title.length
+      ? 'Title length too long'
+      : 'Missing Title and/or Description field'
+    generateClientError({
+      res,
+      action: 'updating',
+      status: 400,
+      messageDetails,
     })
+  } else {
+    updateTask({ title, description, completed, id })
+      .then((result: QueryResult<Task>) =>
+        isEmpty(result.rows)
+          ? generateClientError({ res, action: 'updating', id, status: 404 })
+          : res.status(200).json({ result })
+      )
+      .catch((err) => generateServerError({ res, action: 'updating', err, id }))
+  }
 }
 
 export const deleteTask = (req: Request, res: Response) => {
   const id = parseInt(req.params.id)
-  removeTask(id)
-    .then((result) =>
-      isEmpty(result.rows)
-        ? res.status(404).json({
-            message: `Task ID ${id} was not found in DB`,
-          })
-        : res.status(200).json({ message: `Task ${id} has been removed` })
-    )
-    .catch((err) => {
-      res.status(500).json({
-        err,
-        message: `Error deleting task with id ${id}`,
-      })
+
+  if (!id) {
+    generateClientError({
+      res,
+      action: 'deleting',
+      status: 400,
+      messageDetails: 'missing id',
     })
+  } else {
+    removeTask(id)
+      .then((result) =>
+        isEmpty(result.rows)
+          ? generateClientError({ res, action: 'updating', id, status: 404 })
+          : res.status(200).json({ message: `Task ${id} has been removed` })
+      )
+      .catch((err) => generateServerError({ res, action: 'deleting', err, id }))
+  }
 }
---
+++ src/helpers/errors.ts
@@ -0,0 +1,43 @@
+import type { Response } from 'express'
+
+type Action = 'fetching' | 'creating' | 'updating' | 'deleting'
+
+interface ErrorPayload {
+  res: Response
+  action: Action
+  id?: number
+  err?: string
+  status?: number
+  messageDetails?: string
+}
+
+export const generateServerError = ({ res, action, err, id }: ErrorPayload) => {
+  const message = !id
+    ? `Error ${action} tasks`
+    : `Error ${action} task with id of ${id}`
+
+  return res.status(500).json({ err, message })
+}
+
+export const generateClientError = ({
+  res,
+  action,
+  id,
+  status,
+  messageDetails,
+}: ErrorPayload) => {
+  switch (status) {
+    case 400:
+      res.status(400).json({
+        message: `Malformed Payload while ${action}: ${messageDetails}`,
+      })
+      break
+    case 404:
+      res
+        .status(404)
+        .json({ message: `Not Found while ${action} task: ID ${id}` })
+      break
+    default:
+      res.status(400).json({ message: 'unknown client error' })
+  }
+}
