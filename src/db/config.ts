006327bb8ad503d8496949e9ad58c3a1ab0ebac4
dlevenson44 <daniellevenson4@gmail.com>
2023-11-29 16:14:00 -0500

    fix fly deploy and db config

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,14 +3,21 @@ import { Pool } from 'pg'
 
 dotenv.config()
 
+const connectionConfig =
+  process.env.DB_HOST === 'localhost'
+    ? {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+      }
+    : { connectionString: process.env.DATABASE_URL }
+
 const dbConfig = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
+  ...connectionConfig,
 })
 
 export default dbConfig
--- src/db/migration.mjs
+++ src/db/migration.mjs
@@ -6,7 +6,7 @@ import pg from 'pg'
 
 dotenv.config()
 
-const databaseUrl = process.env.DB_CONNECTION_STRING
+const databaseUrl = process.env.DATABASE_URL
 const pool = new pg.Pool({ connectionString: databaseUrl })
 
 if (process.env.NODE_ENV !== 'production') {
